423940a556c8e3c48b4d2fb7afed42761baa6f31
Mattti Wirtz <119162325+matti377@users.noreply.github.com>
2023-05-23 11:39:11 +0200

    Update ftpbackup.sh

--- ftpbackup.sh
+++ ftpbackup.sh
@@ -1,100 +1,69 @@
 #!/bin/bash
-# File System Backups via FTP with MySQL Databases
-# Copyright (c) 2017 Mitchell Krog <mitchellkrog@gmail.com>
-# ---------------------------------------------------------------------
+#install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+#set up tailscale for remote access
+sudo tailscale up 
 
-# Save as /bin/ftpbackup.sh and make executable
-# chmod +x /bin/ftpbackup.sh
-
-# Requires NCFTP to be installed
-# sudo apt-get install ncftp
-
-# Full backup day Mondays (otherwise incremental backups are done) cen be changed below
-
-# Automatic cleaning up of anything older than 35 days (can be changed to suit you)
-
-### Your System Settings ###
-
-DIRS="/bin /etc /home /var/local /usr/local/bin /usr/lib /var/www"
-BACKUP=/tmp/backup.$$
-NOW=$(date +"%Y-%m-%d")
-INCFILE="/root/tar-inc-backup.dat"
-DAY=$(date +"%a")
-FULLBACKUP="Mon"
-
-### Your MySQL Settings ###
-MUSER="root"
-MPASS="yourpassword"
-MHOST="localhost"
-MYSQL="$(which mysql)"
-MYSQLDUMP="$(which mysqldump)"
-GZIP="$(which gzip)"
-
-### Your FTP server Settings ###
-FTPD="//backup-directory-on-ftp-server"
-FTPU="ftp-username"
-FTPP="ftp-password"
-FTPS="ftp.server.address"
-NCFTP="$(which ncftpput)"
-
-### Your Email Address ###
-EMAILID="youremail@yourdomain.com"
-
-### Backup our DPKG Software List ###
-dpkg --get-selections > /etc/installed-software-dpkg.log
-
-### Start the Backup for the file system ###
-[ ! -d $BACKUP ] && mkdir -p $BACKUP || :
-
-### Check if we want to make a full or incremental backup ###
-if [ "$DAY" == "$FULLBACKUP" ]; then
-  FTPD="//full-backups"
-  FILE="MyServer-fs-full-$NOW.tar.gz"
-  tar -zcvf $BACKUP/$FILE $DIRS
+# Check if Tailscale is already authenticated
+if ! tailscale status | grep -q "Authenticating"; then
+  echo "Tailscale is already authenticated."
 else
-  i=$(date +"%Hh%Mm%Ss")
-  FILE="MyServer-fs-incremental-$NOW-$i.tar.gz"
-  tar -g $INCFILE -zcvf $BACKUP/$FILE $DIRS
-fi
+  # Open Tailscale login page for authentication
+  tailscale authlogin
+  
+  # Wait for authentication to complete
+  echo "Waiting for authentication..."
+  while tailscale status | grep -q "Authenticating"; do
+    sleep 1
+  done
+  
+  echo "Authentication complete."
 
-### Start the MySQL Database Backups ###
-### Get all the MySQL databases names
-DBS="$($MYSQL -u $MUSER -h $MHOST -p$MPASS -Bse 'show databases')"
-for db in $DBS
-do
- FILE=$BACKUP/mysql-$db.gz
- $MYSQLDUMP --single-transaction -u $MUSER -h $MHOST -p$MPASS $db | $GZIP -9 > $FILE
-done
+# Get the Tailscale IP address
+tailscale_ip=$(tailscale status | grep "IP: " | awk '{print $2}')
 
+#make config file
+sudo mkdir /deletewhenyouseeme
 
-### Check the Date for Old Files on FTP to Delete
-REMDATE=$(date --date="35 days ago" +%Y-%m-%d)
+# Specify the file name and path
+file_path="/deletewhenyouseeme/infos.txt"
 
-### Start the FTP backup using ncftp
-ncftp -u"$FTPU" -p"$FTPP" $FTPS<<EOF
-cd $FTPD
-cd $REMDATE
-rm -rf *.*
-cd ..
-rmdir $REMDATE
-mkdir $FTPD
-mkdir $FTPD/$NOW
-cd $FTPD/$NOW
-lcd $BACKUP
-mput *
-quit
+# Save the IP address to the file
+echo "$tailscale_ip" > "$file_path"
+
+# display a message indicating the IP address has been saved
+echo "Tailscale IP address saved to $file_path."
+
+#set up jellyfin
+sudo rm /etc/apt/sources.list.d/jellyfin.list
+
+curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
+
+wget -O- https://repo.jellyfin.org/install-debuntu.sh | sudo bash
+
+sudo apt install curl gnupg
+
+sudo add-apt-repository universe
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/jellyfin.gpg
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
+Types: deb
+URIs: https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )
+Suites: $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )
+Components: main
+Architectures: $( dpkg --print-architecture )
+Signed-By: /etc/apt/keyrings/jellyfin.gpg
 EOF
 
+sudo apt update
 
-### Find out if ftp backup failed or not ###
-if [ "$?" == "0" ]; then
- rm -f $BACKUP/*
- mail  -s "MYSERVER - BACKUP SUCCESSFUL" "$EMAILID"
-else
- T=/tmp/backup.fail
- echo "Date: $(date)">$T
- echo "Hostname: $(hostname)" >>$T
- echo "Backup failed" >>$T
- mail  -s "MYSERVER - BACKUP FAILED" "$EMAILID" <$T
- rm -f $T
-fi
+sudo apt install jellyfin
+
+sudo systemctl start jellyfin
+
+sudo systemctl enable jellyfin
+
+sudo ufw allow (port)
+
